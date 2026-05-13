bd963ef05641028585c209db59b9e700e5a92179
David Jesus Enciso Guadarrama <djenciso@gmail.com>
2026-05-13 12:31:46 -0600

    fix ajuste a config

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -6,10 +6,13 @@ from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
+from app.config import settings
 from app.database import Base
 from app.models import User, Tanda, Participant, Round, Payment
 
+
 config = context.config
+config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
